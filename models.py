1a90a113996d07883b77a58f2f40bf0c51f03627
Tony Tong <tony@theleagueapp.co>
2018-10-21 16:01:30 +0800

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# Not being used now, only using if app.py gets too big and I needa separate into components
 
 from flask_sqlalchemy import SQLAlchemy
 
