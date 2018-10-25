e2c71ae49febddc168667461db8b1b11f7c4e702
Tony Tong <tony@theleagueapp.co>
2018-10-25 17:06:32 +0800

    Added unofficial reviews table

--- models.py
+++ models.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from run import app
+# In heroku __main__ has to be run. Lots of hassle.
+from __main__ import app
+# from run import app 
+# ^ In Heroku Only
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 
@@ -12,17 +16,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ytsnozrropwoym:611808c63f96c
 db = SQLAlchemy(app)
 engine = create_engine('postgres://ytsnozrropwoym:611808c63f96c74d7b755cebc8857cabac5b94215097046256d8cd6855323254@ec2-174-129-236-147.compute-1.amazonaws.com:5432/d7qvs0a0ed4uf')
 
-class Post(db.Model):
-    __tablename__ = 'post'
-    id = db.Column(db.Integer(), primary_key=True)
+##Tables
 
-    title = db.Column(db.String(80), unique=True)
-    post_text = db.Column(db.String(255))
+class unofficial_reviews(db.Model):
+    __tablename__ = 'unofficial_reviews'
 
-    def __init__(self, id, title, post_text):
-        self.id = id
-        self.title = title
-        self.post_text = post_text
+    review_id = db.Column(db.String(100), primary_key=True)
+    mod_class_id = db.Column(db.String(80))
+    review_text = db.Column(db.String(1500))
+
+    def __init__(self, review_id, mod_class_id, review_text):
+        self.review_id = review_id
+        self.mod_class_id = mod_class_id
+        self.review_text = review_text
 
 class official_reviews(db.Model):
     __tablename__ = 'official_reviews'
