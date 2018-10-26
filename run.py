17814bc77666ad72037ec2db46e395cdbe1110e8
Tony Tong <tony@theleagueapp.co>
2018-10-26 20:38:20 +0800

    Update run.py

--- run.py
+++ run.py
@@ -38,21 +38,23 @@ def test2():
 
 @app.route('/test')
 def test():
-        query = "select avg(m1), avg(m2) from official_reviews;"
-        b = db.session.execute(query).fetchall()
-        a = ''
-        for rowproxy in b:
-            a = a + ',' + {"id": rowproxy[0]}.__str__()
-            a = a[1:]    
-        result = json.dumps(a)
-        return Response(result, mimetype='application/json')
+    resultproxy = db.session.execute("select count(*) from official_reviews;").fetchall()
+    d, a = {}, {}
+    for rowproxy in resultproxy:
+        for tup in rowproxy.items():
+            d = {**d, **{tup[0]: tup[1]}}
+        a.update(d)
+    return Response(json.dumps(a), mimetype='application/json')
 
 @app.route('/test4')
 def test4():
-    b = db.session.execute("select count(*) from unofficial_reviews;").fetchall()
-    for rowproxy in b:
-        a = jsonify(rowproxy[0]) # for now is 1 string and 1 value
-    return a
+    resultproxy = db.session.execute("select count(*) from official_reviews;").fetchall()
+    d, a = {}, {}
+    for rowproxy in resultproxy:
+        for tup in rowproxy.items():
+            d = {**d, **{tup[0]: tup[1]}}
+    a.update(d)
+    return Response(json.dumps(a), mimetype='application/json')
 
 @app.route('/<mod_id>/<chart>')
 def avgm1(mod_id, chart):
