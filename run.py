86795d9244398033903ec22aeca2bc3bc52b9a9b
Tony Tong <tony@theleagueapp.co>
2018-10-26 21:38:27 +0800

    Updated json to simplejson to remove decimal problems

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 
 from flask import Flask, request, jsonify
+import simplejson as json
 import requests
-import json
+#import json
 import sys
 
 # Settings
@@ -28,7 +29,7 @@ def hello_module(user_id):
 
 @app.route('/test')
 def test():
-    resultproxy = db.session.execute("select count(*), avg(m1) from official_reviews;").fetchall()
+    resultproxy = db.session.execute("select round(avg(m2), 2), count(*) from official_reviews;").fetchall()
     d, a = {}, {}
     for rowproxy in resultproxy:
         for tup in rowproxy.items():
@@ -36,15 +37,15 @@ def test():
         a.update(d)
     return Response(json.dumps(a), mimetype='application/json')
 
-@app.route('/test4')
-def test4():
-    resultproxy = db.session.execute("select count(*), avg(m1)  from official_reviews;").fetchall()
+@app.route('/test2')
+def test2():
+    resultproxy = db.session.execute("select round(avg(m2), 2) from official_reviews;").fetchall()
     d, a = {}, {}
     for rowproxy in resultproxy:
         for tup in rowproxy.items():
             d = {**d, **{tup[0]: tup[1]}}
-    a.update(d)
-    return Response(json.dumps(a), mimetype='application/json')
+        a.update(d)
+    return Response(json.dumps(a), mimetype='application/json')    
 
 @app.route('/<mod_id>/<chart>')
 def avgm1(mod_id, chart):
@@ -52,11 +53,22 @@ def avgm1(mod_id, chart):
         code = "'" + mod_id + "'"
         query = "select avg(m1) from official_reviews where mod_class_id = " + code
         b = db.session.execute(query).fetchall()
-        a = ''
-        for rowproxy in b:
-            a = a + ',' + {"id": rowproxy[0]}.__str__()
-            a = a[1:]    
-        return jsonify(a)
+        d, a = {}, {}
+    for rowproxy in resultproxy:
+        for tup in rowproxy.items():
+            d = {**d, **{tup[0]: tup[1]}}
+        a.update(d)
+    return Response(json.dumps(a), mimetype='application/json')
 
 if __name__ == '__main__':
     app.run()
+
+
+#Resultproxy docs:
+#class sqlalchemy.engine.RowProxy(parent, row, processors, keymap)
+#Proxy values from a single cursor row.
+#Mostly follows “ordered dictionary” behavior, mapping result values to the string-based column name, the integer position of the result in the row, as well as Column instances which can be mapped to the original Columns that produced this result set (for results that correspond to constructed SQL expressions).
+#has_key(key) Return True if this RowProxy contains the given key.
+#items() Return a list of tuples, each tuple containing a key/value pair.
+#keys() Return the list of keys as strings represented by this RowProxy.
+#https://stackoverflow.com/questions/20743806/sqlalchemy-execute-return-resultproxy-as-tuple-not-dict
